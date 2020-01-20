1cbcff23d627bd278dbf5c20888b47d1d64291db
hyunsoojung <jhs695@naver.com>
2020-01-20 14:54:19 +0900

    supplier

--- src/main/java/functional/Application.java
+++ src/main/java/functional/Application.java
@@ -1,48 +1,47 @@
 package functional;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.Predicate;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
 
 public class Application {
     public static void main(String[] args) {
-        Predicate<Integer> isPositive = i -> i > 0;
 
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, -1, -3);
-        List<Integer> positiveNumbers = new ArrayList<>();
-        for (Integer num : numbers) {
-            if (isPositive.test(num)) {
-                positiveNumbers.add(num);
-            }
-        }
+        long start = System.currentTimeMillis();
+        printValid(true, getVeryExpensiveValue());
+        printValid(false, getVeryExpensiveValue());
+        printValid(false, getVeryExpensiveValue());
+        System.out.println((System.currentTimeMillis() - start) / 1000 + "초 걸린다.");
 
-        System.out.println(positiveNumbers);
+        start = System.currentTimeMillis();
+        printSupplierValid(true, () -> getVeryExpensiveValue());
+        printSupplierValid(false, () -> getVeryExpensiveValue());
+        printSupplierValid(false, () -> getVeryExpensiveValue());
 
-        Predicate<Integer> isPositiveLessthanThree = i -> i > 3;
-
-        List<Integer> numbers2 = Arrays.asList(1, 2, 3, 4, 5, -1, -3, 1);
-        List<Integer> positiveLessThanThreeNumbers = new ArrayList<>();
-        for (Integer num : numbers2) {
-            if (isPositiveLessthanThree.test(num)) {
-                positiveLessThanThreeNumbers.add(num);
-            }
-        }
-
-        System.out.println(positiveLessThanThreeNumbers);
-
-        System.out.println(filter(numbers, i -> i > 0));
-        System.out.println(filter(numbers2, i -> i > 3));
-
+        System.out.println((System.currentTimeMillis() - start) / 1000 + "초 걸린다.");
     }
 
-    public static <T> List<T> filter(List<T> inputs, Predicate<T> predicate) {
-        List<T> returns = new ArrayList<>();
-        for (T input : inputs) {
-            if (predicate.test(input)) {
-                returns.add(input);
-            }
+    public static String getVeryExpensiveValue() {
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        return returns;
+        return "test";
+    }
+
+    public static void printValid(Boolean isTrue, String value) {
+        if (isTrue) {
+            System.out.println(value);
+            return;
+        }
+        System.out.println("Invalid");
+    }
+
+    public static void printSupplierValid(Boolean isTrue, Supplier<String> value) {
+        if (isTrue) {
+            System.out.println(value.get());
+            return;
+        }
+        System.out.println("Invalid");
     }
 }
